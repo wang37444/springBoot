84a490446d9b0c5c64c7e5ffb4272cc6156e6b59
will <will.wang@.uaf.com.cn>
2017-09-28 17:15:42 +0800

    新增redis及log功能

--- sping-boot-sample1/src/test/java/com/will/shedule/SpingBootSample1ApplicationTests.java
+++ sping-boot-sample1/src/test/java/com/will/shedule/SpingBootSample1ApplicationTests.java
@@ -2,15 +2,34 @@ package com.will.shedule;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.will.utils.redis.RedisClientTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpingBootSample1ApplicationTests {
 
+	protected static Logger logger = LoggerFactory.getLogger(RedisClientTemplate.class);
+
+	@Autowired
+	RedisClientTemplate redisClientTemplate;
+
+	/**
+	 * redis测试方法
+	 * 
+	 * 作者：will
+	 * 日期：2017年9月28日下午3:52:40
+	 */
 	@Test
-	public void contextLoads() {
+	public void testRedis() {
+		redisClientTemplate.setWithExpireTime("user", "01", "will", 60);
+		String str = redisClientTemplate.get("user", "01");
+		logger.info("redis缓存信息：" + str);
 	}
 
 }
